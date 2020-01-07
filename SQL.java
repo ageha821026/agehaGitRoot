2eaf4214380056020ec12567c04af30b8021a6ab
ageha821026 <59597188+ageha821026@users.noreply.github.com>
2020-01-07 16:10:58 +0900

    ageha821026

--- SQL.java
+++ SQL.java
@@ -3,13 +3,14 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class SQL {
+	Public String harin = "harin";
 	public void main (Connection con) throws SQLException {
-		//CRUD_Q ÄÃ·³¿¡ ?·Î ÃßÃâ 
+		//CRUD_Q Ã„ÃƒÂ·Â³Â¿Â¡ ?Â·ÃŽ ÃƒÃŸÃƒÃ¢ 
 		
                 String table = "sss from USER_TABLE";
 		con.prepareStatement(table);
 		
-		//DynamicSQLPattern ÃßÃâ 
+		//DynamicSQLPattern ÃƒÃŸÃƒÃ¢ 
 		String dynamic = "{call PR_TOUCH_USER(?)}";
 		con.prepareStatement(dynamic);
 		
